e581b63c976768ecd04a881298528901125dabfe
FLight944 <tamikomaconhada@gmail.com>
2021-09-13 20:59:24 -0300

    adicionado projeto especial em python. PFV N LEIA, DE VDD NÃO É PRA LER

---
+++ py/va.py
@@ -0,0 +1,9 @@
+#oi
+print("vavavava");
+#vavavava;
+vava = ["vava", "vava", "vava", "vava", "vava", "vava", "vava", "vava", "vava", "vava", "vava", "vava"];
+
+for x in vava:
+    print(x);
+# esqueci como se comenta em python
+print("vava.");
---
+++ py/viadometro.py
@@ -0,0 +1,19 @@
+x = int(input("digite um numero "))
+if x == 24 :
+    print("você é muito viado hein\n")
+elif x == 12 :
+    print("ok você é um pouco baitola.\n")
+elif x == 420 :
+    print("haha maconha hur hur maconha\n")
+elif x == 69 :
+    print("haaaaa numero engraçado nice\n")
+elif x == 1488 :
+    print("wtf banido!\n")
+elif x == 89 :
+    print("nada aconteceu nesse ano. Foi só um ano comum ok.\n")
+#elif(x == "15 de Abril de 1989"):
+#    print("Esse dia foi apenas um dia comum ok. \n");
+#elif(x == "10 de Janeiro"):
+#    print("Feliz aniversário guizera. \n");
+else:
+    print("ok ok ta tudo sob controle... Você é macho...")
