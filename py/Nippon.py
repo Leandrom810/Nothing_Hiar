59a660b0db23b84d9f3966291a3a4f001c34adc5
FLight944 <tamikomaconhada@gmail.com>
2021-09-16 20:30:50 -0300

    feito mais coisinhas em cobrinha, e agora fiquei depressivo bicho...

---
+++ py/Nippon.py
@@ -0,0 +1,8 @@
+print("mano...\n");
+ano = input("quantos anos você tem atualmente?\n");
+X = 90 - int(ano);
+dias = X * 365;
+semanas = X * 52;
+meses = X * 12;
+
+print(f"você tem {dias} dias, {semanas} semanas, e {meses} meses até alcançar o ano em que fará 90.");
