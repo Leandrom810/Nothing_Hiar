23d3f554995a000063c57135940ef05d15508a32
FLight944 <tamikomaconhada@gmail.com>
2021-09-16 00:19:15 -0300

    Começado Intensivão de Piton :flushed: FeelsGoodMan NothingHiar

---
+++ py/vava.py
@@ -0,0 +1,4 @@
+print("Bem vindo ao gerador de homem fofo.");
+nome1 = input("Digite o nome de um youtuber (foque em youtuber de minecraft)\n");
+nome2 = input("Agora digite um adjetivo\n");
+print(nome1 + " O " + nome2 + " Do Y.T");
