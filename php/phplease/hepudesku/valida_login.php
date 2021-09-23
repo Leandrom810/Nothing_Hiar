a57d8bd9f4ff8fddadb405f5846f25a5021de243
FLight944 <tamikomaconhada@gmail.com>
2021-09-22 23:04:07 -0300

    agora vamos usar php phplease phpls phpls phpls

---
+++ php/phplease/hepudesku/valida_login.php
@@ -0,0 +1,26 @@
+<?php 
+    $users = array(
+                                                        #gurasen é legitimo. Gura é filha do Forsen.
+        array('email' => "forsen@baj.com", "password" => "gurasen"),
+        array("email" => "funkypudimcat@gmail.com", "password" => "amonge")
+    );
+    /*echo "<pre>";
+    print_r($users);
+    echo "</pre>";
+    */
+    $CHECK = true;
+    foreach($users as $user_app){
+        if($_POST["email"] == $user_app["email"] && $_POST["password"] == $user_app["password"]){
+            
+            break;
+        } else{
+            $CHECK = false;
+        }
+    }
+    if($CHECK == false){
+        header("Location: index.php?login=USER NOT FOUND FORSENCD");
+    } else{
+        header("Location: home.php");
+    }
+
+?>
