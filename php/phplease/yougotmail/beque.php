35bc5937754795f6f0807bbf809f04b6344f443e
FLight944 <tamikomaconhada@gmail.com>
2021-09-29 23:05:10 -0300

    dado halt pq eu n tenho numero de telefone extra pra ficar usando nisso lol

--- php/phplease/yougotmail/beque.php
+++ php/phplease/yougotmail/beque.php
@@ -1,6 +1,8 @@
 <?php
     //importing packages
-    require("./libs/PHPMailer/*");
+    require("./libs/PHPMailer/src/PHPMailer.php");
+    require("./libs/PHPMailer/src/Exception.php");
+    require("./libs/PHPMailer/src/SMTP.php");
     include_once("php/mail.php");
     //importing classes
     use PHPMailer\PHPMailer\PHPMailer;
@@ -26,32 +28,32 @@
         //imagine having to copy and paste a code hahhaahahah what a shame...
             
         //Server settings
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        $mail->SMTPDebug = 2;                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+        $mail->Host       = 'smtp-relay.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'user@example.com';                     //SMTP username
-        $mail->Password   = 'secret';                               //SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        $mail->Username   = 'monky';                     //SMTP username
+        $mail->Password   = 'monkey';                               //SMTP password
+        $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-        
+
         //Recipients
-        $mail->setFrom('from@example.com', 'Mailer');
-        $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
+        $mail->setFrom('donipifa@altmails.com', 'Mailer');
+        //$mail->addAddress('donipifa@altmails.com', 'Lmao');     //Add a recipient
         $mail->addAddress('ellen@example.com');               //Name is optional
-        $mail->addReplyTo('info@example.com', 'Information');
-        $mail->addCC('cc@example.com');
-        $mail->addBCC('bcc@example.com');
+        //$mail->addReplyTo('donipifa@altmails.com', 'Response');
+        //$mail->addCC('cc@example.com');
+        //$mail->addBCC('bcc@example.com');
 
         //Attachments
-        $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-        $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+        //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+        //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
         //Content
         $mail->isHTML(true);                                  //Set email format to HTML
-        $mail->Subject = 'Here is the subject';
-        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        $mail->Subject = 'sadge você copiou e colou códigos...';
+        $mail->Body    = 'sadge <b>monkaE!</b>';
+        $mail->AltBody = 'COPY AND PASTE THIS 5 TIME OR HE WILL COME AFTER YOU MonkaOMEGA';
 
         $mail->send();
         echo 'Message has been sent';
