82ac2f2b45134a2774d2777403e1220f1e15178b
FLight944 <tamikomaconhada@gmail.com>
2021-09-29 23:08:27 -0300

    Sim mais uma vez... Mais uma vez... Mais uma vez... forsenInsane forsenInsane forsenInsane

---
+++ php/phplease/algumblog/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Algum Blog</title>
+</head>
+<body>
+    <h1>Imagine a blog here lmao... Ill do just what I most hate in web development... INTERFACE BUILDING HAHAHAHAHAH LET THE TORTURE BEGIN</h1>
+
+</body>
+</html>
