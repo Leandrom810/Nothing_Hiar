cec07634bda36f25c1fa6ad380ee199083eab319
FLight944 <tamikomaconhada@gmail.com>
2021-10-03 17:21:01 -0300

    Estou deixando de virar macaco monkaOMEGA

--- cpp/plushplush/fake.cpp
+++ cpp/plushplush/fake.cpp
@@ -2,7 +2,7 @@
 
 class Mom{
 public:
-  void mom(){
+  virtual  void message(){
     std::cout << "Im chat's mom \n";
 
   }
@@ -10,7 +10,7 @@ public:
 };
 class Chat : public Mom{
 public:
-  void chat(){
+  void message(){
     std::cout << "gachiGASM clap gachiGASM \n";
 
   }
@@ -18,19 +18,27 @@ public:
 };
 class Fors : public Chat{
 public:
-  void fors(){
+  void message(){
     std::cout << "Im chat's Streamer \n";
 
   }
 
 };
 
+void answer(Mom * m){
+  m->message();
+}
+
 int main(int argc, char const *argv[]) {
   Mom chatmom;
   Chat chat;
   Fors farsen;
-  chatmom.mom();
-  chat.chat();
-  farsen.fors();
+  chatmom.message();
+  chat.message();
+  farsen.message();
+  std::cout<<std::endl << std::endl;
+  answer(&chatmom);
+  answer(&chat);
+  answer(&farsen);
   return 0;
 }
---
+++ cpp/plushplush/forsenInsane.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+class Any{
+private:
+  std::string eu = ".";
+public:
+  void Leandro(){
+    std::cout<<"vavavava ";
+  }
+  Any(){
+    std::cout<<"hi ";
+  }
+  Any(std::string x){
+    this->eu = x;
+  }
+  Any(int x, int y){
+    std::cout<< " Resultado: " << x * y << "\n :ratinho:";
+  }
+  ~Any(){
+    std::cout<<"eu sou " << eu <<" tchau\n";
+  }
+};
+
+int main(int argc, char const *argv[]) {
+  Any a("gay");
+  a.Leandro();
+  std::cout << "gachiGASM gachiGASM gachiGASM";
+  return 0;
+}
