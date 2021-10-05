8fcfacbcb0db03b6855d07cad253008ab84d8bf9
FLight944 <tamikomaconhada@gmail.com>
2021-10-04 22:50:30 -0300

    Feito padronizaçao das loja

---
+++ php/phplease/lojadoforsen/loja.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    <title>Lojinha do Forsen</title>
+  </head>
+  <body>
+    <div id="Lmenu">
+      <div class="content">
+        <div>
+          <a class="btn btn-outline-secondary btn-lg" href="Home.php">
+            Home
+          </a>
+          <a class="btn btn-outline-secondary btn-lg" href="loja.php">
+            Loja
+          </a>
+          <a class="btn btn-outline-secondary btn-lg" href="contato.php">
+            Contato
+          </a>
+        </div>
+      </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <div class="shop">
+      <div class="start">
+
+          <div class="shopitem">
+            <div class="title">
+              title
+            </div>
+            <div class="image">
+              <img src="src/img/download.png" width="50px" height="50px" alt="">
+            </div>
+            <div class="price">
+              R$24,00
+            </div>
+          </div>
+          <div class="shopitem">
+            <div class="title">
+              title
+            </div>
+            <div class="image">
+              <img src="src/img/download.png" width="50px" height="50px" alt="">
+            </div>
+            <div class="price">
+              R$24,00
+            </div>
+          </div>
+          <div class="shopitem">
+            <div class="title">
+              title
+            </div>
+            <div class="image">
+              <img src="src/img/download.png" width="50px" height="50px" alt="">
+            </div>
+            <div class="price">
+              R$24,00
+            </div>
+          </div>
+          <div class="shopitem">
+            <div class="title">
+              title
+            </div>
+            <div class="image">
+              <img src="src/img/download.png" width="50px" height="50px" alt="">
+            </div>
+            <div class="price">
+              R$24,00
+            </div>
+          </div>
+          <div class="shopitem">
+            <div class="title">
+              title
+            </div>
+            <div class="image">
+              <img src="src/img/download.png" width="50px" height="50px" alt="">
+            </div>
+            <div class="price">
+              R$24,00
+            </div>
+          </div>
+          <div class="shopitem">
+            <div class="title">
+              title
+            </div>
+            <div class="image">
+              <img src="src/img/download.png" width="50px" height="50px" alt="">
+            </div>
+            <div class="price">
+              R$24,00
+            </div>
+          </div>
+
+          <div class="shopitem break">
+            <div class="title">
+              title
+            </div>
+            <div class="image">
+              <img src="src/img/download.png" width="50px" height="50px" alt="">
+            </div>
+            <div class="price">
+              R$245,00
+            </div>
+          </div>
+          <div class="shopitem">
+            <div class="title">
+              title
+            </div>
+            <div class="image">
+              <img src="src/img/download.png" width="50px" height="50px" alt="">
+            </div>
+            <div class="price">
+              R$24,00
+            </div>
+          </div>
+      </div>
+      <br>
+      <br>
+      <div class="start">
+        <div class="shopitem">
+          <div class="title">
+            title
+          </div>
+          <div class="image">
+            <img src="src/img/download.png" width="50px" height="50px" alt="">
+          </div>
+          <div class="price">
+            R$24,00
+          </div>
+        </div>
+        <div class="shopitem">
+          <div class="title">
+            title
+          </div>
+          <div class="image">
+            <img src="src/img/download.png" width="50px" height="50px" alt="">
+          </div>
+          <div class="price">
+            R$24,00
+          </div>
+        </div>
+        <div class="shopitem">
+          <div class="title">
+            title
+          </div>
+          <div class="image">
+            <img src="src/img/download.png" width="50px" height="50px" alt="">
+          </div>
+          <div class="price">
+            R$24,00
+          </div>
+        </div>
+        <div class="shopitem">
+          <div class="title">
+            title
+          </div>
+          <div class="image">
+            <img src="src/img/download.png" width="50px" height="50px" alt="">
+          </div>
+          <div class="price">
+            R$24,00
+          </div>
+        </div>
+        <div class="shopitem">
+          <div class="title">
+            title
+          </div>
+          <div class="image">
+            <img src="src/img/download.png" width="50px" height="50px" alt="">
+          </div>
+          <div class="price">
+            R$24,00
+          </div>
+        </div>
+        <div class="shopitem">
+          <div class="title">
+            title
+          </div>
+          <div class="image">
+            <img src="src/img/download.png" width="50px" height="50px" alt="">
+          </div>
+          <div class="price">
+            R$24,00
+          </div>
+        </div>
+
+        <div class="shopitem break">
+          <div class="title">
+            title
+          </div>
+          <div class="image">
+            <img src="src/img/download.png" width="50px" height="50px" alt="">
+          </div>
+          <div class="price">
+            R$245,00
+          </div>
+        </div>
+        <div class="shopitem">
+          <div class="title">
+            title
+          </div>
+          <div class="image">
+            <img src="src/img/download.png" width="50px" height="50px" alt="">
+          </div>
+          <div class="price">
+            R$24,00
+          </div>
+        </div>
+      </div>
+
+      </div>
+
+    </div>
+  </body>
+</html>
