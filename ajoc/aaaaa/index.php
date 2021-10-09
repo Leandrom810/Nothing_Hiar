b11bf182290c5732f305ad8007332261f1003fb1
FLight944 <tamikomaconhada@gmail.com>
2021-10-08 22:24:47 -0300

    feito mais passos para mexer com REDACTED

---
+++ ajoc/aaaaa/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<script src="script/index.js" charset="utf-8"></script>
+
+    <title>Aaaaa</title>
+</head>
+<body>
+    <nav class="navbar-light bg-light mb-4">
+        <div class="container">
+            <div class="navbar-brand mb-0 h1">
+                <h3>
+                    Xing Xong
+                </h3>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+
+    </div>
+</body>
+</html>
