4893dfe26189e00b0447e727dc3f12732d4fb672
FLight944 <tamikomaconhada@gmail.com>
2021-10-09 23:14:06 -0300

    feito teste com query e sincronia com o banco de dados :)

--- php/pdo/teste/index.php
+++ php/pdo/teste/index.php
@@ -1,9 +1,23 @@
 <?php
+    require("obj/query.php");
     $dsn = "mysql:host=localhost;dbname=pdo";
     //FUI HOCKIADO
     
     try{
         $connection = new PDO($dsn, "root", "...");
+        $myQuery = $connection->query(selectById(7));
+
+        echo "<pre>";
+            print_r($myQuery->fetch(PDO::FETCH_OBJ)->email);
+        echo "</pre>";
+
+        /*foreach ($myQuery->fetch(PDO::FETCH_OBJ) as $result){
+            echo "<pre>";
+            print_r($result->email);
+            echo "</pre>";
+        }*/
+
+      
     } catch(PDOException $e){
         echo "Erro: " . $e->getCode() . "<br>" . "Mensagem: " . $e->getMessage();
     }
