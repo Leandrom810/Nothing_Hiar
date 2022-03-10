a581ee2752e4a9b153b3e9061e6935fe8cd8b911
FLight944 <tamikomaconhada@gmail.com>
2022-03-09 21:24:59 -0300

    começado flood, digo, projeto com java, só java mesmo

--- java/chad/chad.java
+++ java/chad/chad.java
@@ -1,4 +1,4 @@
-package java.chad;
+package chad;
 
 
 public class chad{
---
+++ java/try/projeto1/src/Program.java
@@ -0,0 +1,14 @@
+import domain.*;
+
+public class Program {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Person p1 = new Person(1, "Leandro", "funkypudimcat@gmail.com");
+        Person p2 = new Person(2, "Leandrotm17bolsonaro", "funkypudimpatriotacat@gmail.com");
+        Person p3 = new Person(3, "Leandropopularsocial", "funkypudimsocialistacat@gmail.com");
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+        
+    }
+}
