7d697d15fb1145093ab6324ca5072fbef806aacc
FLight944 <tamikomaconhada@gmail.com>
2022-03-17 00:40:35 -0300

    Vasco

--- soyscript/fun/proj/functions.js
+++ soyscript/fun/proj/functions.js
@@ -1,7 +1,58 @@
+const fs = require("fs");
+const path = require("path");
+
 function test(){
     console.log("lulululu");
 }
 
-module.exports = {
-    test
+function readDir(pathvar){  
+    return new Promise((files, reject)=>
+    {try {
+        let fileslisted = fs.readdirSync(pathvar);
+        const toReturn = fileslisted.map(file => path.join(pathvar, file));
+        files(toReturn);
+
+    } catch(e){
+        reject(e);
+    }
+    })
 }
+
+function readFile(pathvar){
+    return new Promise((files, reject) =>{
+        try{
+            const content = fs.readFileSync(pathvar, {encoding: "utf-8"});
+            files(content.toString());
+        }catch(e){
+            reject(e);
+        }
+
+    })
+}
+
+function readFiles(varpath){
+    return Promise.all(varpath.map(path=> readFile(path)))
+}
+
+function endWith(array, varwith){
+    return array.filter(element => element.endsWith(varwith));
+}
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+    test,
+    readDir,
+    endWith,
+    readFile,
+    readFiles
+}
+
--- soyscript/fun/proj/index.js
+++ soyscript/fun/proj/index.js
@@ -1,4 +1,8 @@
-const myFunctions = require('./functions');
+const mF = require('./functions');
+// mF significa "My Functions"
+const path = require('path');
 
+const dirpath = path.join(__dirname, 'data', 'sub');
 
-myFunctions.test();
+mF.readDir(dirpath).then(files => mF.endWith(files, ".srt")).then(fileSRT=>mF.readFiles(fileSRT)).then(console.log);
+
