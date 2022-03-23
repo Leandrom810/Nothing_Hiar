4a631513a6c59de5cea771c179c1df4a664bf963
Remiliano944 <tamikomaconhada@gmail.com>
2022-03-22 23:53:03 -0300

    commitzinha pra não perder o ritmo rsrs

---
+++ soyscript/fun/concepts/pure.js
@@ -0,0 +1,9 @@
+//Função pura é uma função cujo retorno é causado pelos valores de entrada.
+// não há efeitos colaterais.
+
+const PI = 3.14
+
+
+const area = (raio)=> Math.sqrt(raio) * PI
+
+console.log(area(10));
