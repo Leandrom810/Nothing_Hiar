768b4112114431ac7f94498afdb9509069456d80
Remiliano944 <tamikomaconhada@gmail.com>
2022-03-23 01:52:58 -0300

    Hello Mr Freeman em React

--- soyscript/React/try/src/components/functions.js
+++ soyscript/React/try/src/components/functions.js
@@ -1,5 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
 const sum = (a, b)=>a+b;
 
+function clock(){
+    const element=(
+        <div>
+            <h1>Hello Mr Freeman.</h1>
+            <h2>Look at the time! it is {new Date().toLocaleTimeString()}</h2>
+        </div>
+    )
+    ReactDOM.render(element, document.getElementById("root"));
+}
+
 export{
-    sum
+    sum,
+    clock
 }
--- soyscript/React/try/src/index.js
+++ soyscript/React/try/src/index.js
@@ -3,12 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+const mF = require("../src/components/functions.js")
+
+//versão otimizada de elemento único!
+const element = React.createElement(
+  "h1",
+  {className:"greeting"},
+  "Greetings Mr.Freeman!"
+);
+
+//ESSE EXEMPLO NÃO É INSERIDO POIS O REACT É INTELIGENTE O BASTANTE PARA NÃO INSERIR MEDIANTE A POSSIBILIDADE DE CAUSAR BUGS!! :D
+// const element = React.createElement(
+//   "div",
+//   {className: "greetings"},
+//   "h1",
+//   {className: "title"},
+//   "HELLO THERE FRIEND!!! IM FROM INDIA AND I LOVE PEOPLE!!!!"
+// );
+
+//versão não otimizada de elemento único!
+// const element = (
+//   <h1 className="greeting">
+//     This is not optmized, Mr Freeman.
+//   </h1>
+// );
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  // <h1>Olá mundo!</h1>,
+  // element,
+  // document.getElementById('root')
+  setInterval(mF.clock, 1000)
+
 );
 
 // If you want to start measuring performance in your app, pass a function
